2a687ab11046b57d984146e44be5e6bac5fe36d4
Amit Bhardwaj <growwyouwithus@gmail.com>
2025-12-22 13:50:34 +0530

    update backend2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     VitePWA({
@@ -10,9 +11,17 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
         cleanupOutdatedCaches: true
-      }
+      },
+      manifest: false
     })
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   server: {
     port: 5173,
     strictPort: false
